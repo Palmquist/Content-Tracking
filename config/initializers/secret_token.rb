6e9b50144936bb70bf7e66273d75fa2027d500b6
Reid Palmquist <reid.palmquist@gmail.com>
2013-08-13 10:55:16 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContentTracker::Application.config.secret_key_base = 'bb29d65ba0906f90e8a369291e7a672017bec53c0910d7ba03df2a198f64ef1f8aef462a8d14a9961696c514887ecf89fa90fb324c479a8a003f2251f56af2de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ContentTracker::Application.config.session_store :cookie_store, key: '_Content Tracker_session'
